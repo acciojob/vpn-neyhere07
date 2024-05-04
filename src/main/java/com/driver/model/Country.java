683301b7121e73718e67c1267fa3183861052d74
neyhere07 <nyananayak27.com>
2024-05-04 15:11:31 +0530

    vpn web

--- src/main/java/com/driver/model/Country.java
+++ src/main/java/com/driver/model/Country.java
@@ -1,1 +1,87 @@
 // Note: Do not write @Enumerated annotation above CountryName in this model.
+
+package com.driver.model;
+
+import org.springframework.data.repository.cdi.Eager;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Country {
+    private int countryId;
+
+    private CountryName countryName;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+
+    @OneToOne
+    @JoinColumn
+    private ServiceProvider serviceProvider;
+
+
+    public Country() {
+    }
+
+    public Country(CountryName countryName, String code) {
+        this.countryName = countryName;
+        this.code = code;
+    }
+
+    public Country(int countryId,
+                   ServiceProvider serviceProvider,
+                   User user,
+                   String code,
+                   CountryName countryName) {
+        this.countryId = countryId;
+        this.serviceProvider = serviceProvider;
+        this.user = user;
+        this.code = code;
+        this.countryName = countryName;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public CountryName getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(CountryName countryName) {
+        this.countryName = countryName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ServiceProvider getServiceProvider() {
+        return serviceProvider;
+    }
+
+    public void setServiceProvider(ServiceProvider serviceProvider) {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
